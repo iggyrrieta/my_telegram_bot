7471dab5861090e83de2abb319b1774f8fbd8b74
iggy <lorente.inaki@gmail.com>
2020-05-22 13:19:51 +0200

    online done, js working : index.html, botinspector.py

--- py_bot/CumpleCarla/src/botinspector.py
+++ py_bot/CumpleCarla/src/botinspector.py
@@ -1,23 +1,59 @@
 import re
-from flask import Flask, render_template   
-
-app = Flask(__name__,
-            template_folder="../www/templates",
-            static_folder="../www/static")
+from flask import Flask, render_template  
+from apscheduler.schedulers.background import BackgroundScheduler 
+from flask_socketio import SocketIO, emit
 
 #=================================================================
-# FLASK : SHOW LOG
+# VARIABLES
 #=================================================================
-@app.route("/")
-def index():
+num_rows = 0
+last_num_rows = 0
+
+#=================================================================
+# CONFIG
+#=================================================================
+app = Flask(__name__, template_folder="../www/templates")
+
+socketio = SocketIO(app)
+
+#=================================================================
+# SCHEDULE JOBS
+#=================================================================
+def job():
     log_data = []
+    global num_rows
+    global last_num_rows
+
     with open("../cfg/app.log", 'r') as file:  
         for line in file:
             #Timestamp = pos 0, data = pos 3
             linia = [re.split(' - ', line)[0], re.split(' - ', line)[3]]
             log_data.append(linia)
-          
-    return render_template("index.html", log = log_data)
+            num_rows += 1
+            
+    
+    if num_rows > last_num_rows:
+        #job emits on websocket
+        socketio.emit('logger',log_data, broadcast=True)
+        last_num_rows = num_rows
+        num_rows = 0
+    else:
+        num_rows = 0
 
+scheduler = BackgroundScheduler()
+running_job = scheduler.add_job(job, 'interval', seconds=1, max_instances=1)
+scheduler.start()
+
+#=================================================================
+# PUBLISH
+#=================================================================
+@app.route("/")
+def index():          
+    return render_template("index.html")
+
+
+#=================================================================
+# MAIN
+#=================================================================
 if __name__ == '__main__':
-    app.run(use_reloader = True, debug = True)
+    socketio.run(app, host='0.0.0.0')
